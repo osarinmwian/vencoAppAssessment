9dfc2a8601f78901be6be8274447023c8ee91ea1
osarinmwian <noel.osarinmwian@gmail.com>
2023-04-20 14:19:15 +0100

    set storage

--- src/screen/home/index.tsx
+++ src/screen/home/index.tsx
@@ -154,7 +154,7 @@ export default function HomeScreen() {
       } else {
         return incomingCallNumber;
       }
-    }, 20000);
+    }, 40000);
 
     handleEncryptNumber();
     AppState.addEventListener("change", (nextAppState) => {
